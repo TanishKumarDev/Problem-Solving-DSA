94ab07640560e3e9782ce48e1a688b0e5604a621
Tanish <tanish.kumar.contact@gmail.com>
2025-07-10 17:12:41 +0530

    refactor(Transform Array by Parity): streamline comments and improve code clarity

--- Sorting Techniques/3467. Transform Array by Parity.cpp
+++ Sorting Techniques/3467. Transform Array by Parity.cpp
@@ -1,24 +1,19 @@
-// LeetCode Problem: 3467. Transform Array by Parity
+#include <bits/stdc++.h>
+using namespace std;
 
-/*
-Replace each even number with 0.
-Replace each odd numbers with 1.
-Sort the modified array in non-decreasing order.
+// 🟢 Approach 3 Optimal: 
 
-Return the resulting array after performing these operations.
+// Intuition : 
+// 1. in result even number(which are 0s) should come first, how many times? exactly 'No of even numbers' in nums. similarly 
+// 2. Odd come after all even numbers count, how many times? exactly 'No of odd numbers' in nums.
+// thus 
+// 3. we can count the number of even numbers in the array and then fill the result array with 0s for even numbers and 1s for odd numbers.
 
---------------------------------------------------------
-Read Question twice. 
-
-1. in result even number(which are 0s) should come first, how many times? exactly 'No of even numbers' in nums. similarly 
-2. Odd come after all even numbers count, how many times? exactly 'No of odd numbers' in nums.
-
-thus 
-3. we can count the number of even numbers in the array and then fill the result array with 0s for even numbers and 1s for odd numbers.
---------------------------------------------------------
-Time Complexity: O(n)
-Space Complexity: O(n)
-*/
+// Algo :
+// 1. Count the number of even numbers in the array.
+// 2. Create a new array of the same size as the input array.
+// 3. Fill the new array with 0s for even numbers and 1s for odd numbers.
+// 4. Return the new array.
 
 class Solution {
 public:
@@ -52,3 +47,9 @@ public:
     return res;
     }
 };
+
+int main()
+{
+    
+    return 0;
+}
